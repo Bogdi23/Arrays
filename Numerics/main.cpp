f05484ddb84168c215e3b6ca9116778240fa557c
bogdant14@mail.ru <bogdant14@mail.ru>
2025-04-30 15:50:37 +0300

    Numerics encoding

---
+++ Numerics/main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+//#define BINARY
+//#define HEX
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	
+#ifdef BINARY
+	int decimal; //эта переменная будет хранить десятичное число, введённое пользователем
+	cout << "Введите десятичное число: "; cin >> decimal;
+	const int n = 32; //максимально-возможная разрядность двоичного числа
+	bool binary[n] = {}; //этот массив будет хранить разряды двоичного числа
+	int i = 0;
+	for (; decimal; decimal >>= 1)
+		binary[i++] = decimal & 1;
+
+	for (--i; i >= 0; i--)
+	{
+		cout << binary[i];
+		if (i % 8 == 0)cout << " ";
+		if (i % 4 == 0)cout << " ";
+	}
+	cout << endl;
+#endif // BINARY
+
+#ifdef HEX
+	int decimal;
+	cout << "Введите десятичное число: "; cin >> decimal;
+	const int n = 8;
+	char hex[n] = {};
+	int i = 0;
+	for (; decimal; decimal /= 16)
+	{
+		hex[i++] = decimal % 16;
+	}
+
+	for (--i; i >= 0; i--)
+	{
+		cout << char(hex[i] < 10 ? hex[i] + 48 : hex[i] + 55);
+	}
+	cout << endl;
+#endif // HEX
+
+}
---
+++ Project1/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int ROWS = 3;
+	const int COLS = 4;
+	int arr[ROWS][COLS];
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			bool unique;
+			do 
+			{
+				arr[i][j] = rand() % (ROWS * COLS);
+				unique = true;
+				for (int k = 0; k <= i; k++)
+				{
+					for (int l = 0; l < (k < i ? COLS : j); l++)
+					{
+						if (arr[i][j] == arr[k][l])
+						{
+							unique = false;
+							break;
+						}
+					}
+					if (!unique)break;
+				}
+			} while (!unique);
+		}
+	}
+}
