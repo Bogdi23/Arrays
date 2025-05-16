51df531d18a03bfaeb6ccf46fd69055512e6f83f
bogdant14@mail.ru <bogdant14@mail.ru>
2025-05-16 20:07:18 +0300

    Repeats2 DONE

--- Repeat2/main.cpp
+++ Repeat2/main.cpp
@@ -26,7 +26,7 @@ void main()
 	}
 	cout << endl;
 
-	for (int i = 0; i < ROWS; i++)
+	/*for (int i = 0; i < ROWS; i++)
 	{
 		for (int j = 0; j < COLS; j++)
 		{
@@ -54,6 +54,27 @@ void main()
 			}
 			if (count) cout << "Значение " << arr[i][j] << " повторяется " << count << " раз." << endl;
 		}
+	}*/
+
+	for (int i = 0; i < ROWS * COLS; i++)
+	{
+		bool met_before = false;
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[0][i] == arr[0][j])
+			{
+				met_before = true;
+				break;
+			}	
+		}
+		if (met_before)continue;
+		int count = 0;
+		for (int j = i + 1; j < ROWS * COLS; j++)
+		{
+			if (arr[0][i] == arr[0][j])count++;
+		}
+		if (count) cout << "Значение " << arr[0][i] << " повторяется " << count << " раз." << endl;
 	}
+
 	cout << "Finita la comedia";
 }
