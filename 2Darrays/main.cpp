0f3ff6773ed929e8e7221323d614d4f785b13861
bogdant14@mail.ru <bogdant14@mail.ru>
2025-05-19 00:42:58 +0300

    2Darrays sort

---
+++ 2Darrays/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	
+	cout << "2Darrays" << endl;
+	const int ROWS = 5;
+	const int COLS = 4;
+	int arr[ROWS][COLS];
+
+	//Заполнение слечайными числами:
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+	
+	//Вывод исходного массива на экран:
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	//Сортировка массива:
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			for (int k = i; k < ROWS; k++)
+			{
+				for (int l = k > i ? 0 : j + 1; l < COLS; l++)
+				{
+					//arr[i][j] - выбранный элемент
+					//arr[k][l] - перебираемый элемент
+					if (arr[k][l] < arr[i][j])
+					{
+						int buffer = arr[i][j];
+						arr[i][j] = arr[k][l];
+						arr[k][l] = buffer;
+					}
+				}
+			}
+		}
+	}
+
+	//Вывод отсортированного массива на экран:
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
